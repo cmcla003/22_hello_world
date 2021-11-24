922c92609fb2bd02ddec57be1e4deabf7aa19100
cmcla003 <mclarenc@awatapu.school.nz>
2021-11-25 09:26:31 +1300

    accessing values

---
+++ 00_lists.py
@@ -0,0 +1,13 @@
+# Create lists
+score = [12,20,34,6,0]
+name = ["Tom","Sam", "Charlotte", "Neal", "Amanda"]
+
+
+# output score and names
+print(score)
+print(score[2])
+print(name[0],score[0])
+print(name[1],score[1])
+print(name[2],score[2])
+print(name[3],score[3])
+print(name[4],score[4])
