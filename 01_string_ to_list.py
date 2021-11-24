dc19f573f182f0ea154e0b3541364966e3299bc4
cmcla003 <mclarenc@awatapu.school.nz>
2021-11-25 09:26:39 +1300

    accessing values

---
+++ 01_string_ to_list.py
@@ -0,0 +1,10 @@
+# Variables
+sentence = input("Please write a sentence: ")
+
+# create lists from input
+word_list = [sentence.split()]
+letter_list = list(sentence)
+
+# Output information
+print(word_list)
+print(letter_list)
