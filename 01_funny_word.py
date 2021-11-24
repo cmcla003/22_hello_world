9515750a2838e0087136a8799d85097e946d91ef
cmcla003 <mclarenc@awatapu.school.nz>
2021-11-25 09:27:58 +1300

    string to lists and format

---
+++ 01_funny_word.py
@@ -0,0 +1,13 @@
+# Variables
+funny_word ="snollygoster"
+message = "Politicians are snollygosters"
+
+# Print 6th letter
+print(funny_word[5])
+
+# define as a list
+funny_list = list(funny_word)
+
+# print each letter of the variable
+print(funny_list)
+print(message.split())
