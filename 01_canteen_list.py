1a4f0b8faa487e8f5ae9f377def733cef0726301
cmcla003 <mclarenc@awatapu.school.nz>
2021-11-25 09:27:37 +1300

    create and output list

---
+++ 01_canteen_list.py
@@ -0,0 +1,14 @@
+food_list= ["sandwich", "mince pie", "hot chips", "donut"]
+print(food_list)
+
+food_list.append("milkshake")
+print(food_list)
+
+food_list.insert(0,"filled roll")
+print(food_list)
+
+food_list.pop(1)
+print(food_list)
+
+food_list.remove("hot chips")
+print(food_list)
